57ba9a1ef7edc463c9e8b74d1da2931af8288f1b
Qberthat <quentin-berthat@web.de>
2019-08-02 14:35:20 +0200

    Added 30s countdown && disabled Submit if no answer is selected

--- app/controllers/quiz.js
+++ app/controllers/quiz.js
@@ -2,7 +2,6 @@ import Controller from '@ember/controller';
 import { inject } from '@ember/service';
 import { on } from '@ember/object/evented';
 import { computed } from '@ember/object';
-import { run, later } from '@ember/runloop';
 
 export default Controller.extend({
 	store: inject(),
@@ -17,16 +16,24 @@ export default Controller.extend({
 	quotMark: "'",
 	questionnaireLength: 15,
 	responseMessage:'',
-	seconds: 0,
+	seconds: 30,
 	start: null,
 	interval: null,
 
-	loadQuestions: on('init', function () {
+	startTimer: on('init', function() {
+		const controller = this;
+		setInterval(function() {
+			controller.set('seconds', controller.get('seconds') - 1);
+			if (controller.get('seconds') === 0)  controller.send('selectNextQuestion');
+		}, 1000);
+	}),
+
+	loadQuestions: on('init', function() {
 		const controller = this;
 		return this.quizManager.getAllQuizes(this.get('questionnaireLength'))
 			.then((questions) => {
 				questions.results.forEach((question) => {
-					controller.store.createRecord('question', question)
+					controller.store.createRecord('question', question);
 				});
 				controller.set('questions', controller.store.peekAll('question').toArray());
 			});	
@@ -36,31 +43,31 @@ export default Controller.extend({
 		this.set('user', this.userManager.getUser());
 	}),
 
-	answers: computed('questionIndexGreaterThan', function () {
-		if(this.get('questionIndexGreaterThan') > this.get('questionnaireLength')){
+	answers: computed('questionIndexGreaterThan', function() {
+		if(this.get('questionIndexGreaterThan') > this.get('questionnaireLength')) {
 			return  this.answerManager.getAnswers();
 		} 
 		return [];
 	}),
 
-	questionIndexGreaterThan: computed("currentQuestionIndex", function(){
+	questionIndexGreaterThan: computed("currentQuestionIndex", function() {
 		let nextIndex = this.get('currentQuestionIndex');
 		return (nextIndex + 1);
 	}),
 
-	questionnaireScore: computed('questionnaireLength', function(){
+	questionnaireScore: computed('questionnaireLength', function() {
 		let totalScore = this.get('questionnaireLength');
 		return parseInt(totalScore * 2);
 	}),
 
-	timer: computed('seconds', function(){
-		const that = this;
+	/**timer: computed('currentQuestionIndex', function(){
+		const _this = this;
 		let seconds = this.get('seconds')
-		if (that.interval === null) {
-			that.set('start', Date.now());
-			that.interval = setInterval(function() {
-				const start = that.get('start');
-				that.set('seconds', Date.now() - start);
+		if (_this.interval === null) {
+			_this.set('start', Date.now() + 1000*31);
+			_this.interval = setInterval(function() {
+				const start = _this.get('start');
+				_this.set('seconds', start - Date.now() );
 			}, 1000);
 		}
 		return parseInt(seconds / 1000);
@@ -81,7 +88,7 @@ export default Controller.extend({
 
 		selectNextQuestion() {
 			this.set('currentQuestionIndex', this.get('currentQuestionIndex') + 1);
-			this.get('timer');
+			this.set('seconds', 30);
 		},
 
 		answerQuestion(question, answer) {
